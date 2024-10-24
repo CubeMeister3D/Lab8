b12b42d281ab4fc100a53bd8906a8ef647052d37
CubeMeister3D <146042583+CubeMeister3D@users.noreply.github.com>
2024-10-23 19:48:07 -0600

    fixed the deleteCity() Method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -82,6 +82,13 @@ public class CustomList extends ArrayAdapter<City> {
      *  throws an error if the city that we are trying to remove from the list is not actually in the list
      */
     public void deleteCity(City city){
-
+        if (!this.hasCity(city)){
+            throw new IllegalArgumentException();
+        }
+        int i;
+        for(i = 0; i < cities.size();i++){
+            if(cities.get(i).equals(city)){break;}
+        }
+        cities.remove(i);
     }
 }
